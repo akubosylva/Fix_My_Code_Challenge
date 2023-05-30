9f787dc557f0b9bc1dc9d1f13d0296a9e2efac6a
akubosylva <akubosylva@gmail.com>
2023-05-31 00:05:05 +0100

    Fix_My_Code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -45,6 +45,17 @@ class Square():
 
 
 if __name__ == "__main__":
+    """
+    This code block is executed only when the script is run directly (not imported as a module).
+
+    The __name__ attribute is a built-in variable in Python that represents the name of the current module or script.
+    When a script is run directly, the __name__ attribute is set to "__main__", indicating that it is the main module being executed.
+
+    This condition is often used to separate the code that should only run when the script is executed directly, as opposed to being imported as a module.
+
+    Example usage:
+        python script.py
+    """
 
     # Create a Square object with width=12 and height=9
     s = Square(width=12, height=9)
